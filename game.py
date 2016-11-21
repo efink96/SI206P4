419c482889675faa080c5ded536085debb908f54
Emma Fink <etfink@umich.edu>
2016-11-21 16:31:38 -0500

    added basket, apple, banana, and carrot classes

--- game.py
+++ game.py
@@ -3,10 +3,103 @@ import sys
 
 import pygame
 
-# top-down shooter, probably
+# character will be on the ground the whole time (able to move horizontally), and they will catch as much fruit as possible in 60 seconds
 from pygame.locals import K_LEFT, K_RIGHT, K_SPACE, Rect
 
 X_MAX = 600
 Y_MAX = 600
 
 everything = pygame.sprite.Group()
+
+class Basket(pygame.sprite.Sprite):
+	def __init__(self):
+		super(Basket, self).__init__()
+		self.image = pygame.image.load("basket.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = (X_MAX/2, Y_MAX - 30)
+		self.dx = 0
+		self.dy = 0
+
+	def steer(self, direction, operation):
+		v = 10
+		if operation == START:
+			if direction in (LEFT, RIGHT):
+				self.dx = {LEFT: -v, RIGHT: v}[direction]
+		if operation == STOP:
+			if direction in (LEFT, RIGHT):
+				self.dx = 0
+
+class Apple(pygame.sprite.Sprite):
+	def __init__(self, y_pos):
+		super(Apple, self).__init__()
+		self.image = pygame.image.load("apple.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = (0, y_pos)
+		self.dx = 0
+		self.dy = 0
+		self.velocity = 7
+
+	def update(self):
+		def update(self):
+        x, y = self.rect.center
+
+        if y > Y_MAX:
+            x, y = random.randint(0, X_MAX), 0
+            self.velocity = 7
+        else:
+            x, y = x, y + self.velocity
+
+        self.rect.center = x, y
+
+ 	def caught_in_basket(self):
+ 		super(Apple, self).kill()
+
+class Banana(pygame.sprite.Sprite):
+	def __init__(self, y_pos):
+		super(Banana, self).__init__()
+		self.image = pygame.image.load("banana.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = (0, y_pos)
+		self.dx = 0
+		self.dy = 0
+		self.velocity = 7
+
+	def update(self):
+		def update(self):
+        x, y = self.rect.center
+
+        if y > Y_MAX:
+            x, y = random.randint(0, X_MAX), 0
+            self.velocity = 7
+        else:
+            x, y = x, y + self.velocity
+
+        self.rect.center = x, y
+
+ 	def caught_in_basket(self):
+ 		super(Banana, self).kill()
+
+class Carrot(pygame.sprite.Sprite):
+	def __init__(self, y_pos):
+		super(Carrot, self).__init__()
+		self.image = pygame.image.load("carrot.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = (0, y_pos)
+		self.dx = 0
+		self.dy = 0
+		self.velocity = 7
+
+	def update(self):
+		def update(self):
+        x, y = self.rect.center
+
+        if y > Y_MAX:
+            x, y = random.randint(0, X_MAX), 0
+            self.velocity = 7
+        else:
+            x, y = x, y + self.velocity
+
+        self.rect.center = x, y
+
+ 	def caught_in_basket(self):
+ 		super(Carrot, self).kill()
